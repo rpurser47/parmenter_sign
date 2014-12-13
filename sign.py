d019b55e4fdbebfa5bd48ab635f3ca6df852d051
rpurser47 <rob@purser.org>
2014-12-13 12:06:00 +0000

    Added POST (power on self-test). Added variable delay for update.

--- sign.py
+++ sign.py
@@ -3,18 +3,52 @@ import urllib2
 import time
 import serial
 
+try:
+    port = serial.Serial("/dev/ttyAMA0", baudrate = 9600, timeout=3.0)
+    port.write("+90000000-")
+    time.sleep(0.4)
+    port.write("+09000000-")
+    time.sleep(0.4)
+    port.write("+00900000-")
+    time.sleep(0.4)
+    port.write("+00090000-")
+    time.sleep(0.4)
+    port.write("+00009000-")
+    time.sleep(0.4)
+    port.write("+00000900-")
+    time.sleep(0.4)
+    port.write("+00000090-")
+    time.sleep(0.4)
+    port.write("+00000009-")
+    time.sleep(0.4)
+    port.write("+11111111-")
+    port.close()
+except:
+    print("Warning -- had an error.")
+
 url = "https://www.kimonolabs.com/api/24afmnvi?apikey=j0qCMngQnLkG8EI6t7gRkwrW8KfTaTgN"
 
 maxValue = 380000
 minValue = -100000
 segments = 8
-period = 120
+period = 5
+previousLevel = -1
 
 while 1:
     try:
         jsonThermo = json.loads(urllib2.urlopen(url).read())
 
         currentLevel = int(jsonThermo['results']['collection1'][0]['current'])
+
+        if currentLevel != previousLevel:
+            valueChangedAt = time.clock()
+            previousLevel = currentLevel
+
+        period = (time.clock() - valueChangedAt) / 10
+        if period < 5:
+            period = 5
+        elif period > 900:
+            period = 900
 
         segmentsToLight = int(round(float(currentLevel - minValue)/float(maxValue - minValue) * segments))
 
@@ -41,7 +75,6 @@ while 1:
         port.write(command)
         port.close()
     except:
-        print("Warning -- had an error: ")
-        port.close()
+        print("Warning -- had an error.")
 
     time.sleep(period)
